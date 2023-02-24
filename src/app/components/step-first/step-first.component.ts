6dfa7d30a891f8bca7f04cdac01425b61259eeca
Murad <mur1d1xmedov@gmail.com>
2023-02-24 15:49:22 +0400

    header nav deactivated

--- src/app/components/step-first/step-first.component.ts
+++ src/app/components/step-first/step-first.component.ts
@@ -10,9 +10,9 @@ import { DataService } from 'src/app/services/data.service';
 export class StepFirstComponent implements OnInit {
   personalInfo!: FormGroup;
   formGroups = [
-    { label: 'name', placeholder: 'e.g. Stephen King' },
-    { label: 'email', placeholder: 'e.g. stephenking@lorem.com' },
-    { label: 'phone', placeholder: 'e.g. +1 234 567 890' },
+    { label: 'name', placeholder: 'Murad Ahmadov' },
+    { label: 'email', placeholder: 'mur1dehmedov@gmail.com' },
+    { label: 'phone', placeholder: '+994 70 8783006' },
   ];
 
   constructor(private fb: FormBuilder, private dataService: DataService) {}
@@ -26,7 +26,7 @@ export class StepFirstComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       phone: [
         '',
-        [Validators.required, Validators.pattern(/^\+1 \d{3} \d{3} \d{3}$/)],
+        [Validators.required, Validators.pattern(/^\+994 \d{2} \d{7}$/)],
       ],
     });
   }
@@ -37,7 +37,7 @@ export class StepFirstComponent implements OnInit {
     } else if (this.personalInfo.controls[groupLabel].hasError('email')) {
       return 'Please enter a valid email address';
     } else if (this.personalInfo.controls[groupLabel].hasError('pattern')) {
-      return 'Please enter a valid phone number (e.g. +1 234 567 890)';
+      return `Please enter a valid phone number (+994 70 8783006)`;
     } else {
       return '';
     }
